fe02b7944c85ca06c036272f40fda32a5124abc3
Siarhei Chura <s.chura@pin-up.tech>
2025-09-27 17:08:48 +0200

    icons fix

---
+++ src/constants/soc-media-icons.ts
@@ -0,0 +1,28 @@
+export interface SocMediaIcon {
+  name: string;
+  url: string;
+  icon: string;
+}
+
+export const SOC_MEDIA_ICONS: SocMediaIcon[] = [
+  {
+    name: 'linkedin',
+    url: 'https://www.linkedin.com/in/sergei-chura-3a4708215/',
+    icon: 'fa7-brands:linkedin-in',
+  },
+  {
+    name: 'github',
+    url: 'https://github.com/siarheichura',
+    icon: 'fa7-brands:github',
+  },
+  {
+    name: 'telegram',
+    url: 'https://t.me/seryyvolk',
+    icon: 'fa7-brands:telegram',
+  },
+  {
+    name: 'instagram',
+    url: 'https://www.instagram.com/serikvolk',
+    icon: 'fa7-brands:instagram',
+  },
+];
