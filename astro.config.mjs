189d5a43bbb70044844591ab1a57794ca61c9c77
Siarhei Chura <s.chura@pin-up.tech>
2025-09-27 16:39:57 +0200

    gh pages deploy setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,6 @@ import icon from 'astro-icon';
 export default defineConfig({
   devToolbar: { enabled: false },
   integrations: [icon()],
+  site: 'https://github.com/siarheichura',
+  base: '/portfolio',
 });
